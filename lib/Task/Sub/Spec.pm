3b765677c47a6d88272e0e56ba2d9561417519a4
Steven Haryanto <stevenharyanto@gmail.com>
2012-01-21 03:29:44 +0000

    Import of SHARYANTO/Task-Sub-Spec-0.03 from CPAN.
    gitpan-cpan-distribution: Task-Sub-Spec
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SHARYANTO/Task-Sub-Spec-0.03.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Task/Sub/Spec.pm
+++ lib/Task/Sub/Spec.pm
@@ -1,6 +1,6 @@
 package Task::Sub::Spec;
 
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 
 # todo: Sub::Spec::Exporter
 
@@ -16,55 +16,24 @@ Task::Sub::Spec - Install all Sub::Spec modules
 
 =head1 VERSION
 
-version 0.02
+version 0.03
 
 =head1 DESCRIPTION
 
-This task will install all Sub::Spec modules.
+B<NOTICE>: This module and the L<Sub::Spec> standard is deprecated as of Jan
+2012. L<Rinci> is the new specification to replace Sub::Spec, it is about 95%
+compatible with Sub::Spec, but corrects a few issues and is more generic.
+C<Perinci::*> is the Perl implementation for Rinci and many of its modules can
+handle existing Sub::Spec sub specs.
+
+This bundle is now installing Task::Perinci instead. Sub::Spec modules will be
+removed from CPAN in the future.
 
 =head1 TASK CONTENTS
 
 =head2 Included modules
 
-=head3 L<Sub::Spec>
-
-=head3 L<Sub::Spec::BashComplete>
-
-=head3 L<Sub::Spec::CmdLine>
-
-=head3 L<Sub::Spec::Examples>
-
-=head3 L<Sub::Spec::Gen::ReadTable>
-
-=head3 L<Sub::Spec::GetArgs::Argv>
-
-=head3 L<Sub::Spec::GetArgs::Array>
-
-=head3 L<Sub::Spec::GetArgs::GetPost>
-
-=head3 L<Sub::Spec::HTTP>
-
-=head3 L<Sub::Spec::HTTP::Server>
-
-=head3 L<Sub::Spec::Object>
-
-=head3 L<Sub::Spec::Response::Format>
-
-=head3 L<Sub::Spec::Runner>
-
-=head3 L<Sub::Spec::To::HTML>
-
-=head3 L<Sub::Spec::To::Org>
-
-=head3 L<Sub::Spec::To::Pod>
-
-=head3 L<Sub::Spec::To::Text>
-
-=head3 L<Sub::Spec::URI>
-
-=head3 L<Sub::Spec::URI::http>
-
-=head3 L<Sub::Spec::Use>
+=head3 L<Task::Perinci>
 
 =head1 AUTHOR
 
@@ -72,7 +41,7 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2011 by Steven Haryanto.
+This software is copyright (c) 2012 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
